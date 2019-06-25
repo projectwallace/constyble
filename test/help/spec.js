3cb2d567c5d23c842fd0cd4ee1ebb98a5aa7d716
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2019-06-25 15:30:08 +0200

    update dependencies (#11)
    * update dependencies
    
    * move tap back from devDependencies to dependencies
    
    * update snapshots and include new css-analyzer metric

--- test/help/spec.js
+++ test/help/spec.js
@@ -2,15 +2,15 @@ const test = require('ava')
 const execa = require('execa')
 
 test('it shows a help message when called with --help', async t => {
-	const {stdout, code} = await execa('./lib/cli.js', ['--help'])
+	const {stdout, exitCode} = await execa('./lib/cli.js', ['--help'])
 
 	t.snapshot(stdout)
-	t.is(code, 0)
+	t.is(exitCode, 0)
 })
 
 test('it shows a help message when called without arguments', async t => {
-	const {stdout, code} = await execa('./lib/cli.js', {input: ''})
+	const {stdout, exitCode} = await execa('./lib/cli.js', {input: ''})
 
 	t.snapshot(stdout)
-	t.is(code, 0)
+	t.is(exitCode, 0)
 })
--- test/version/spec.js
+++ test/version/spec.js
@@ -3,15 +3,15 @@ const execa = require('execa')
 const semver = require('semver')
 
 test('it shows a valid version when passing --version', async t => {
-	const {stdout, code} = await execa('./lib/cli.js', ['--version'])
+	const {stdout, exitCode} = await execa('./lib/cli.js', ['--version'])
 
 	t.is(stdout, semver.valid(stdout))
-	t.is(code, 0)
+	t.is(exitCode, 0)
 })
 
 test('it shows a valid version when passing -V', async t => {
-	const {stdout, code} = await execa('./lib/cli.js', ['-V'])
+	const {stdout, exitCode} = await execa('./lib/cli.js', ['-V'])
 
 	t.is(stdout, semver.valid(stdout))
-	t.is(code, 0)
+	t.is(exitCode, 0)
 })
